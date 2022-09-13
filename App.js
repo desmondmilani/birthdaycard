2ea75aceabd5087a80f531bad554b643122d0e49
Desmond <des.godgiven.com>
2022-09-13 16:03:44 +0200

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+
+
+const birthday = {uri: './assets/birthday.jpg'}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+
+    <ImageBackground style={styles.container} source={require('./assets/birthday.jpg')}>
+      <View>
+        <Text style={styles.happy}>Happy</Text>
+        <Text style={styles.birthday}>Birthday</Text>
+        <Text style={styles.to}>To</Text>
+        <Text style={styles.amu}>Amukelani Mandy</Text>
+        <Text style={styles.milani}>Milani</Text>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    textAlign: 'center',
+  },
+  happy: {
+    color: '#aa00ff',
+    fontStyle: 'bold',
+    fontFamily: 'monotype corsiva',
+    fontSize: 50,
+  },
+  birthday: {
+    color: '#ee33ff',
+    fontSize: 26,
+    fontFamily: 'monotype corsiva',
+  },
+  to: {
+    color: '#ff77ff',
+    fontFamily: 'monotype corsiva',
+    fontSize: 16,
+  },
+  amu: {
+    color: '#ffaaff',
+    fontSize: 40,
+    fontFamily: 'impact',
+  },
+  milani: {
+    color: '#ffaaff',
+    fontSize: 40,
+    fontFamily: 'impact',
   },
 });
